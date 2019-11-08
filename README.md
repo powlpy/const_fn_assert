7bfc6e66f02835ecf2fd2e22ac4fbdabe12e3ab3
Nicolas Poelen <powlpy@gmail.com>
2019-11-09 00:23:27 +0100

    add project files

---
+++ README.md
@@ -0,0 +1,31 @@
+# const_fn_assert
+
+[![Build Status](https://travis-ci.com/powlpy/const_fn_assert.svg?branch=master)](https://travis-ci.com/powlpy/const_fn_assert)
+[![Crate](https://img.shields.io/crates/v/const_fn_assert.svg)](https://crates.io/crates/const_fn_assert)
+[![Documentation](https://docs.rs/const_fn_assert/badge.svg)](https://docs.rs/const_fn_assert)
+![Minimum rustc version](https://img.shields.io/badge/rustc-1.31+-yellow.svg)
+[![License](https://img.shields.io/crates/l/const_fn_assert.svg)](https://github.com/powlpy/const_fn_assert/blob/master/LICENSE)
+
+Assertions for `const` function.
+
+- [Documentation](https://docs.rs/const_fn_assert)
+- [Release notes](https://github.com/powlpy/const_fn_assert/releases)
+
+## Example
+
+```rust
+
+```
+
+## Usage
+
+Add this to your `Cargo.toml` :
+
+```toml
+[dependencies]
+const_fn_assert = "0.1"
+```
+
+## TODO list
+
+ - debug assert
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+// Copyright 2019 const_fn_assert Developers
+//
+// Licensed under the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>. This file may not be
+// copied, modified, or distributed except according to those terms.
 
 #![no_std]
 #![doc(html_root_url = "https://docs.rs/const_fn_assert")]
@@ -29,48 +34,3 @@ macro_rules! cfn_assert_ne {
     }
 }
 
-#[cfg(test)]
-mod tests{
-
-    #[test]
-    fn test_assert(){
-        cfn_assert!(true);
-        cfn_assert!(true && (true != false));
-        cfn_assert!((true && true) != false);
-        cfn_assert_eq!(false, false);
-    }
-    
-    const fn sub_fn(x: u8) -> u8 {
-        cfn_assert!(x < 5);
-        x + 1
-    }
-    
-    const _TEST_ASSERT: u8 = sub_fn(1);
-    
-    // _TEST_ASSERT_FAIL is compile-fail
-    
-    #[test]
-    fn test_sub_fn_assert(){
-        let _ = sub_fn(1);
-    }
-    
-    #[test]
-    #[should_panic]
-    fn test_sub_fn_assert_fail(){
-        let _ = sub_fn(6);
-    }
-    
-    #[test]
-    const fn test_const_assert(){
-        const FIVE: usize = 5;
-
-        cfn_assert!(FIVE * 2 == 10);
-        cfn_assert!(FIVE > 2);
-    }
-    
-    #[test]
-    fn test_const_fn_assert(){
-        const TEST_CONST_ASSERT: () = test_const_assert();
-        let _ = TEST_CONST_ASSERT;
-    }
-}
